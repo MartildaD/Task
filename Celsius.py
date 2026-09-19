e2d1f89bd3b60a2d4eaddfccc4fb52bfd1254b95
MartildaD <enioladurosimi43@gmail.com>
2026-09-19 19:49:46 +0100

    Delete Celsius.py

--- Celsius.py
+++
@@ -1,9 +0,0 @@
-celsius_input = float(input("Enter a temperature in celsius: "))
-for temperature  in range(5):
-    celsius = celsius_input + temperature
-
-    if celsius < -273:
-        print(f"{celsius}C is impossible! NOthing can be colder than _273C.")
-    else:
-        farenheit = (celsius * 9/5) + 32
-        print(f"{celsius}C = {farenheit}F")
