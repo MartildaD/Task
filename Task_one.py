4aa058a45e952360ae1dfb2405535b81d561060f
MartildaD <enioladurosimi43@gmail.com>
2026-09-19 19:51:39 +0100

    Delete Task_one.py

--- Task_one.py
+++
@@ -1,9 +0,0 @@
-question_one = input("What is your problem:")
-question_two = bool(input("Have you had this problem before? (yes or no):"))
-if 'yes':
-    print('Well, you have it again.')
-elif 'no':
-    print('Well, you have it now.')
-
-
-
